fe6b2467bc20bdda8da4e10dc380760471f08db0
rashmiths <rashmiths28@gmail.com>
2021-04-22 10:45:25 +0530

    look and clook from shubhanshu

---
+++ disk_scheduling.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void disk_clook()
+{
+	int n, track;
+	cout<<"\nEnter Number Of Requests : ";
+	cin>>n;
+	cout<<"\nEnter Number Of Tracks : ";
+	cin>>track;
+	int req[n+1];
+	cout<<"\nEnter Current Postions of R/W Head : ";
+	cin>>req[0];
+	cout<<"\nEnter the Requests\n";
+	for(int i = 1; i <= n; i++)
+	cin>>req[i];
+	
+	int first = req[0];
+	sort(req, req+n+1);
+	int seek = 0, pos;
+	for(int i = 0; i <= n; i++)
+	{
+		if(req[i] == first)
+			pos = i;
+	}
+	
+	cout<<"\nHead Movement\n";
+	for(int i = pos-1; i >= 0; i--) // left
+	{
+		cout<<req[i+1]<<"->"<<req[i]<<" : "<<abs(req[i+1]-req[i])<<"\n";
+		seek += abs(req[i+1] - req[i]);
+	}
+	
+	if(pos != n) // go to right end
+	{
+		cout<<req[0]<<"--->"<<req[n]<<" : "<<abs(req[n]-req[0])<<"\n";
+		seek += abs(req[n] - req[0]);
+	}
+	
+	for(int i = n; i > pos + 1; i--) // right to left
+	{
+		cout<<req[i]<<"->"<<req[i-1]<<" : "<<abs(req[i]-req[i-1])<<"\n";
+		seek += abs(req[i] - req[i-1]);
+	}
+		
+	cout<<"\nTotal Head Movement : "<<seek;
+}
+
+void disk_look()
+{
+	int n, track;
+	cout<<"\nEnter Number Of Requests : ";
+	cin>>n;
+	cout<<"\nEnter Number Of Tracks : ";
+	cin>>track;
+	int req[n+1];
+	cout<<"\nEnter Current Postions of R/W Head : ";
+	cin>>req[0];
+	cout<<"\nEnter the Requests\n";
+	for(int i = 1; i <= n; i++)
+	cin>>req[i];
+	
+	int first = req[0];
+	sort(req, req+n+1);
+	int seek = 0, pos;
+	for(int i = 0; i <= n; i++)
+	{
+		if(req[i] == first)
+			pos = i;
+	}
+	
+	cout<<"\nHead Movement\n";
+	for(int i = pos-1; i >= 0; i--) // head to left
+	{
+		cout<<req[i+1]<<"->"<<req[i]<<" : "<<abs(req[i+1]-req[i])<<"\n";
+		seek += abs(req[i+1] - req[i]);
+	}
+	
+	if(pos != n) // go to right
+	{
+		cout<<req[0]<<"->"<<req[pos+1]<<" : "<<abs(req[pos+1]-req[0])<<"\n";
+		seek += abs(req[pos+1] - req[0]);
+	}
+	
+	for(int i = pos+2; i <= n; i++) // right to left
+	{
+		cout<<req[i-1]<<"->"<<req[i]<<" : "<<abs(req[i-1]-req[i])<<"\n";
+		seek += abs(req[i-1] - req[i]);
+	}
+		
+	cout<<"\nTotal Head Movement : "<<seek;
+}
